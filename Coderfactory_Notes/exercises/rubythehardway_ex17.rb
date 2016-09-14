70ba39fe63e26e5a51580aa0375ff49c4468229f
jfhinchcliffe <hinchy@gmail.com>
2016-09-14 23:34:12 +1000

    More exercises

--- Coderfactory_Notes/exercises/rubythehardway_ex17.rb
+++ Coderfactory_Notes/exercises/rubythehardway_ex17.rb
@@ -6,3 +6,16 @@ in_file = open(from_file)
 indata = in_file.read
 
 puts "The indata file is #{indata.length} bytes long."
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+
+puts "Ready? Hit RETURN to continue, or CTRL C to abort!"
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done!"
+
+out_file.close
+in_file.close
