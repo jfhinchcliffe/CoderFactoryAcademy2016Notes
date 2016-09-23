97e194561e90735e2d6fd96a6497c47cabf69335
jfhinchcliffe <hinchy@gmail.com>
2016-09-23 11:09:45 +1000

    Up to Ex45 in Ruby the Hard Way. Leaving early to go to a wedding 😶

--- Coderfactory_Notes/exercises/rubythehardway_ex45.rb
+++ Coderfactory_Notes/exercises/rubythehardway_ex45.rb
@@ -1,1 +1,21 @@
 #make a game from scratch
+
+def rps(first_player_throw)
+  rules = [{"rock" => { "win" => "paper", "lose" => "scissors"}},
+          {"paper" => { "win" => "rock", "lose" => "scissors"}},
+          {"scissors" => { "win" => "rock", "lose" => "paper"}}]
+  winner = ""
+  computer = rules[0][0]
+  puts computer
+  rules.each do |item|
+    item.each do |throw, rules|
+      if first_player_throw == throw
+        rules["win"] == computer ? winner = "Computer Wins!" : winner = "Player Wins!"
+        winner = "draw" if first_player_throw == computer
+      end
+    end
+  end
+  puts winner
+end
+
+rps("rock")
