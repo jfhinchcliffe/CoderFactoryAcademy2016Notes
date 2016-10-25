d0f68aa4635ebdf034632f4477727796d369d6df
jfhinchcliffe <hinchy@gmail.com>
2016-10-25 16:27:31 +1100

    Finished for the day

--- Coderfactory_Notes/Week7/magazine/app/controllers/articles_controller.rb
+++ Coderfactory_Notes/Week7/magazine/app/controllers/articles_controller.rb
@@ -1,30 +1,54 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
 
   def new
-
+    # Not needed, but rendering new render 'new'
+    @article = Article.new
   end
 
   def index
     @articles = Article.all
   end
 
+  def edit
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
-
-    @article = Article.find(params[:id])
     @toenail = params
-
   end
 
   private
 
     def article_params
       params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 
 end
